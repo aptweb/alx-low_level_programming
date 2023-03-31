e0f4ac67dbd7d968d6f4d1ae417731d91d4f2310
abdallah <moussaid@gmail.com>
2023-03-30 17:42:08 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,44 +8,31 @@
  */
 char *cap_string(char *str)
 {
-	    int i, j, len;
-    char temp;
+	int index = 0;
 
-    len = strlen(str);
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-    // reverse the entire string
-    for (i = 0, j = len - 1; i < j; i++, j--)
-    {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
-    // reverse each word in the string
-    for (i = 0, j = 0; i < len; i++)
-    {
-        if (str[i] == ' ')
-        {
-            // reverse the current word
-            for (int k = j, l = i - 1; k < l; k++, l--)
-            {
-                temp = str[k];
-                str[k] = str[l];
-                str[l] = temp;
-            }
+		index++;
+	}
 
-            j = i + 1;
-        }
-    }
-
-    // reverse the last word in the string
-    for (int k = j, l = len - 1; k < l; k++, l--)
-    {
-        temp = str[k];
-        str[k] = str[l];
-        str[l] = temp;
-    }
-
-    return str;
-
+	return (str);
 }
